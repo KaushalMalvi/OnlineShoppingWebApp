8333648e660931c54c4489d7f0a914969ad2a9c5
KaushalMalvi <malvikaushal@gmail.com>
2017-12-05 15:25:20 +0530

    Updated - 5-12-17 at 3:25

---
+++ OnlineShoppingTest/src/main/java/com/ksm/OnlineShoppingTest/controller/PageController.java
@@ -0,0 +1,17 @@
+package com.ksm.OnlineShoppingTest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PageController {
+
+	@RequestMapping (value = {"/","/index","/home"})
+	public ModelAndView index() {
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("message", "Welcome To My Web Page..");
+		return mv;
+	}
+}
---
+++ OnlineShoppingTest/src/main/java/com/ksm/OnlineShoppingTest/controller/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Admin
+ *
+ */
+package com.ksm.OnlineShoppingTest.controller;
